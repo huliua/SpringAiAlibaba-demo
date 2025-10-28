b53c6b37f7e7d1517e7ce3cb86e6823c86fbf41d
huliua <1148247580@qq.com>
2025-10-28 21:42:29 +0800

    优化提示词使用方式

--- chatWithRag/src/main/java/com/huliua/springaialibabademo/config/AiConfig.java
+++ chatWithRag/src/main/java/com/huliua/springaialibabademo/config/AiConfig.java
@@ -13,6 +13,7 @@ import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,9 +23,8 @@ public class AiConfig {
     @Resource
     private VectorStore pgVectorStore;
 
-    private static final String PROMPT = """
-            你是一个就职于金智教育的程序员，名字叫小弱智。你可以帮助用户解决JAVA、VUE、JS、CSS等技术上的问题。请以严谨、专业且准确的回答帮助用户解决问题！
-            """;
+    @Value("classpath:/system_prompt.st")
+    private org.springframework.core.io.Resource prompt;
 
     /**
      * 构建聊天内存对象（存储在数据库中）
@@ -57,7 +57,7 @@ public class AiConfig {
                         MessageChatMemoryAdvisor.builder(chatMemory).build(), // 历史消息
                         retrievalAugmentationAdvisor // RAG
                 )
-                .defaultSystem(PROMPT)
+                .defaultSystem(prompt)
                 .build();
     }
 }
